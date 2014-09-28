e7dd355132b4cf1435fe7666187dc2df82414c51
brainmurphy1 <brianmurphy@gatech.edu>
2014-09-27 23:51:25 -0400

    added a landing page with some javascipt animations, a stylesheet, and some placeholders. Niether code nor product are highly polished

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+var jade = require('jade');
 
 app.get('/', function(req, res) {
-	res.send('hello world');
+	var fn = jade.compileFile('index.jade',{});
+	fs.readFile("style.css", null,function (err, data) {
+		res.send(fn({css:data}));
+	});
 });
 
 app.listen(3000);
