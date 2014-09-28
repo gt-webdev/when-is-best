40295eb1d4c078973d0c922f5e7c295685187571
jmhwang7 <jmhwang7@gmail.com>
2014-09-27 22:09:05 -0700

    Added directory structure for static files (public) and jade files (views). Renamed style.css to landing.css. Modified index.jade to link the stylesheet.

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ var app = express();
 var fs = require('fs');
 var jade = require('jade');
 
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'jade');
+
 app.get('/', function(req, res) {
-	var fn = jade.compileFile('index.jade',{});
-	fs.readFile("style.css", null,function (err, data) {
-		res.send(fn({css:data}));
-	});
+	res.render('index', {});
 });
 
 app.listen(3000);
