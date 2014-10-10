3dabae259d26b20107f67b80ef7fa32e8e9979d0
brainmurphy1 <brianmurphy@gatech.edu>
2014-10-09 23:30:52 -0400

    Set up new landing page layout mark up, without date picker controls. Generally follows principles of the Material Design scheme.

--- public/js/index_animation.js
+++ public/js/index_animation.js
@@ -1,59 +1,15 @@
-var mainCanvas = document.querySelector("#myCanvas");
-var mainContext = mainCanvas.getContext("2d");
-
-function drawCircle(radius) {
-
-    // draw the circle
-    mainContext.beginPath();
-
-    mainContext.arc(0, 75, radius, 0, Math.PI * 2, false);
-    mainContext.closePath();
-
-    // color in the circle
-    mainContext.fillStyle = "#00FF00";
-    mainContext.fill();
-}
-$("#dateSelector").change(function(){
-    $("#goBlock").animate(
-        {left:635}, {}, 40);
-    $("#myCanvas").animate(
-        { left:600 },
-        {
-            duration:200,
-            start: function(animation) {
-                var radius = 1;
-                var circleDrawInterval = setInterval(function(){
-                        radius += 2;
-                        drawCircle(radius);
-                        if (radius > 25) {
-                            clearInterval(circleDrawInterval);
-                        }
-                    },20);
-            },
-            complete: function() {
-                var size = 10;
-                var triangleDrawInterval = setInterval(function() {
-
-                    size = size+=5;
-
-                    mainContext.beginPath();
-
-                    mainContext.moveTo(0, mainCanvas.height / 2 - size);
-                    mainContext.lineTo(size * 2, mainCanvas.height / 2);
-                    mainContext.lineTo(0, mainCanvas.height / 2 + size);
-                    mainContext.lineTo(0, mainCanvas.height / 2 - size);
-                    mainContext.strokeStyle = "green";
-                    mainContext.stroke();
-                    mainContext.closePath();
-
-                    // color in the circle
-                    mainContext.fillStyle = "#00FF00";
-                    mainContext.fill();
-                    if (size > 74) {
-                        clearInterval(triangleDrawInterval);
-                    }
-                },20);
-            }
+$(document).ready(function () {
+    $("#oneTimeRadioSelection").click(function () {
+        var $panel = $('#datepickerBlock');
+        if (!$panel.is(':visible')) {
+            $panel.slideDown("slow", "easeOutBounce");
         }
-    );
+    });
+
+    $("#weeklyRadioSelection").click(function () {
+        var $panel = $('#datepickerBlock');
+        if ($panel.is(':visible')) {
+            $panel.slideUp("slow");
+        }
+    });
 });
