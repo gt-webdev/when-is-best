709b954602f090ddc76667260dbb664f8a8a13d2
jmhwang7 <jmhwang7@gmail.com>
2014-10-15 22:09:00 -0400

    Added post functions to create-event page. Added route from landing to create-event page.

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,9 @@ router.get('/create-event', function(req, res) {
   res.render("create_event", {});
 });
 
+router.post('/create-event', function(req, res) {
+  console.log(req.body);
+  res.json({"event-id": 1234324});
+});
+
 module.exports = router;
