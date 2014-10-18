b5c8ad351f7c6b46b8617bb5b6468eb7fa223e64
brainmurphy1 <brianmurphy@gatech.edu>
2014-10-18 17:01:40 -0400

    Can send data to backend

--- public/js/create-event/main.js
+++ public/js/create-event/main.js
@@ -4,13 +4,14 @@ jQuery(function($) {
 			$.ajax({
 				url: "/create-event",
 				type: "POST",
-				data: JSON.stringify(utility.getDates()),
+				data: JSON.stringify(utility.getJSON()),
 				dataType: "json",
 				contentType: "application/json; charset=utf-8",
 				success: function(data) {
 					console.log(data);
 				}
 			});
+			alert(JSON.stringify(utility.getJSON()));
 		}
 	};
 
@@ -18,10 +19,84 @@ jQuery(function($) {
 		getDates: function() {
 			return { "dates": "Hey!"};
 			//return $("#currentData").text();
+		},
+
+		getJSON: function () {
+			var jsonTemplate = {
+				"event_name": "Birthday Party",
+				"event_type": "recurring",
+				"proposed_dates": [
+				],
+				"creator_name": "ME",
+				"creator_email": "trash@thegarbagecan.com",
+				"member_email": ["a@dsf.com", "b@dsf.com"],
+				"description": "this is a test"
+			}
+
+			for (var j = 0; j < 7; j++) {
+				var dateJson = {
+					"date" : "10/" + j + "/2014",
+					"times" : []
+				};
+				var start = null;
+				var end = null;
+				for (var i = 0; i < 24; i++) {
+					if (start == null && enabled[i][j]) {
+						start = i;
+					} else if (start != null && !enabled[i][j]) {
+						end = i - 1;
+						dateJson.times.push(
+							{
+								"start" : start,
+								"end" : end
+							}
+						);
+						start = null;
+						end = null;
+					}
+				}
+				if (start != null) {
+					dateJson.times.push(
+						{
+							"start" : "" + start,
+							"end" : "" + enabled.length - 1
+						}
+					);
+				}
+				if (dateJson.times.length > 0) {
+					jsonTemplate.proposed_dates.push(dateJson);
+				}
+			}
+			return jsonTemplate;
 		}
 	};
 
 	var dates = utility.getDates();
 
 	$("#testPost").click({ "dates": dates }, services.post);
+
+	var enabled = new Array(24);
+	for (var i = 0; i < enabled.length; i++) {
+		enabled[i] = new Array(7);
+		for(var j = 0; j < 7; j++) {
+			enabled[i][j] = false;
+		}
+	}
+
+	$('.timeslot').click(function () {
+		var $button = $(this);
+		var dayIndex = $button.attr("wib-day");
+		var timeIndex = $button.attr("wib-time");
+		if (enabled[timeIndex][dayIndex]) {
+			$button.css("border", "0px solid #000");
+			enabled[timeIndex][dayIndex] = false;
+		} else {
+			$button.css("border", "1px solid #0F0");
+			enabled[timeIndex][dayIndex] = true;
+		}
+	});
+
+	$('#sendButton').click(function (event) {
+		services.post(event);
+	});
 });
