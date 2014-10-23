008cead64eb9f14fee8986a298113bfe580dd86f
jmhwang7 <jmhwang7@gmail.com>
2014-10-23 11:18:29 -0400

    changed /create-event route to /event/create

--- routes/index.js
+++ routes/index.js
@@ -9,22 +9,22 @@ router.get('/', function(req, res) {
   res.render('index', {});
 });
 
-router.get('/create-event', function(req, res) {
-  res.render('create_event', {});
-});
-
-router.post('/create-event', function(req, res) {
-  MongoClient.connect(url, function(err, db) {
-    if(err){
-      console.log('posting error to DB');
-    }
-    db.collection('events').insert(req.body,function(err){
+router.route('/event/create')
+  .get(function(req, res) {
+    res.render('create_event', {});
+  })
+  .post(function(req, res) {
+    MongoClient.connect(url, function(err, db) {
       if(err){
-        console.log('insert error to DB');
+        console.log('posting error to DB');
       }
+      db.collection('events').insert(req.body,function(err){
+        if(err){
+          console.log('insert error to DB');
+        }
+      });
     });
+    console.log('POST successful');
   });
-  console.log('POST successful');
-});
 
 module.exports = router;
