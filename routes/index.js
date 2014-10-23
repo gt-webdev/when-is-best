f2c7abf29586e6d9ac1ed29fa9e6bcc58a3f8096
Trey Washington <lt.washington9@gmail.com>
2014-10-23 02:08:34 -0400

    small clean for debugging

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var mongodb = require('mongodb');
-var client = mongodb.MongoClient;
+var MongoClient = mongodb.MongoClient;
 var url = process.env.MONGOHQ_URL || "mongodb://localhost/whenisbest";
 
-var insertDocs = function(db,docs,collection,callback){
-
-  var collection = db.collection(collection);
-
-  collection.insert(docs,function(err,result){
-    if(err)
-      console.log(err);
-
-    callback(result);
-  });
-}
 
 router.get('/', function(req, res) {
   res.render('index', {});
@@ -25,15 +14,17 @@ router.get('/create-event', function(req, res) {
 });
 
 router.post('/create-event', function(req, res) {
-    console.log(req.body);
-  client.connect(url, function(err, db) {
-    if(err)
-      console.log(err);
-
-    insertDocs(db,req.body,'test',function(){
-      db.close();
+  MongoClient.connect(url, function(err, db) {
+    if(err){
+      console.log('posting error to DB');
+    }
+    db.collection('events').insert(req.body,function(err){
+      if(err){
+        console.log('insert error to DB');
+      }
     });
   });
+  console.log('POST successful');
 });
 
 module.exports = router;
