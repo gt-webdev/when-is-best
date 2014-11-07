73fcecca4b894803ff86c517e4a224e85dbb3d2a
ckucera3 <claytonkucera@gmail.com>
2014-11-06 23:33:27 -0500

    weekly events now stored between user sessions

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var bodyParser = require('body-parser');
 var fs = require('fs');
 var jade = require('jade');
 var config = require('./config');
-
+var session = require("express-session");
+var cookieparser = require("cookie-parser");
+var MongoStore = require('connect-mongo')(session);
 
 var router = require('./routes');
 
@@ -12,8 +14,32 @@ app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'jade');
 app.use(bodyParser.json());
 
+//the ingredients for making cookies
+    app.use(cookieparser());
+    //create a session hooked up to mongodb
+    app.use(session({
+      store: new MongoStore({
+        url: config.mongo.url
+      }),
+      secret: '1234567890QWERTYfd',
+      autoreconnect: true,
+      resave: true,
+      saveUninitialized: true
+    }, function () {
+        console.log("db connection open");
+      }));
+
 //middleware function for router
 app.use(function(req,res,next){
+    
+    //if eventIndex hasn't been created in this session...
+    if(!req.session.eventIndex) {
+        //create an eventIndex and eventArray
+        req.session.eventIndex = 0;
+        req.session.eventArray = [];
+    }
+    
+    //move on to the next function
     next();
 });
 
--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 module.exports = {
   port: process.env.port || 3000,
   mongo:{
-    url: process.env.MONGOHQ_URL || "mongodb://localhost/whenisbest"
+    //url: process.env.MONGOHQ_URL || "mongodb://localhost/whenisbest"
+    url: 'mongodb://test:123@linus.mongohq.com:10090/app30734144' || "mongodb://localhost/whenisbest"
   },
   mandrill_api_key: process.env.MANDRILL_APIKEY || "Your APIKEY"
 };
